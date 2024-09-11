5a85252974cbaeb8a6ef16c476b2e281ff96b0ea
Charles Feng <charles@charles.io>
2024-09-11 10:48:29 -0600

    chore: update license to LGPLv3 (#10)

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,7 +1,18 @@
 /**
- * Copyright Acro Data Solutions, Inc. and other contributors where applicable.
- * Licensed under the BSD 2-Clause License; you may not use this file except in
- * compliance with the BSD 2-Clause License.
+ * Copyright (C) 2024 Acro Data Solutions, Inc.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 const AcroWrapped = Symbol("AcroWrapped");
