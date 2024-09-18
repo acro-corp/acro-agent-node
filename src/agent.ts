01f9c81cb965532a60705974f99016e0d506ed7a
Charles Feng <charles@charles.io>
2024-09-18 16:16:23 -0600

    fix: add ability to ignore response statuses (#26)
    * fix: add ability to ignore response statuses
    
    * only ignroe if actually has a status

--- src/agent.ts
+++ src/agent.ts
@@ -45,6 +45,12 @@ interface TrackOptions {
   // { request: { <type>: <boolean> } }
   // e.g. { request: { query: true, body: false } }
   request?: Record<string, boolean>;
+  // { response: { status: string[], ignoreStatus: string[] } }
+  // e.g. { response: { ignoreStatus: ["401", "404"] } }
+  response?: {
+    status?: string[];
+    ignoreStatus?: string[];
+  };
 }
 
 export type Logger = Record<
@@ -115,7 +121,7 @@ class AcroAgent {
       // try to grab from cwd
       try {
         const name = JSON.parse(
-          readFileSync(join(process.cwd(), "package.json"), "utf-8"),
+          readFileSync(join(process.cwd(), "package.json"), "utf-8")
         )?.name;
 
         if (name) {
@@ -185,7 +191,7 @@ class AcroAgent {
       (this._streamOptions || {
         highWaterMark: 10,
         objectMode: true,
-      }) as WritableOptions,
+      }) as WritableOptions
     );
 
     // initialize what to track w/ defaults
@@ -266,6 +272,19 @@ class AcroAgent {
   }
 
   shouldTrackAction(action: any) {
+    // if response status is in ignore list, or not in explicitly defined include list
+    if (
+      action?.response?.status &&
+      ((this._track?.response?.ignoreStatus?.length &&
+        this._track?.response?.ignoreStatus.includes(
+          action?.response?.status
+        )) ||
+        (this._track?.response?.status?.length &&
+          !this._track?.response?.status.includes(action?.response?.status)))
+    ) {
+      return false;
+    }
+
     // if any changes occurred, and track methods were not specifically configured
     if (
       action?.changes?.length &&
@@ -278,7 +297,7 @@ class AcroAgent {
     if (
       this._track?.actions?.[action?.action?.type]?.methods &&
       !this._track?.actions?.[action?.action?.type]?.methods?.includes(
-        action?.action?.verb,
+        action?.action?.verb
       )
     ) {
       return false;
@@ -347,14 +366,14 @@ class AcroAgent {
     hookType: string,
     exports: T,
     name: string,
-    basedir: string | undefined | void,
+    basedir: string | undefined | void
   ) {
     let version: string = "";
 
     if (basedir) {
       try {
         version = JSON.parse(
-          readFileSync(join(basedir, "package.json"), "utf-8"),
+          readFileSync(join(basedir, "package.json"), "utf-8")
         )?.version as string;
       } catch (err) {
         // do nothing
@@ -362,7 +381,7 @@ class AcroAgent {
     }
 
     this.logger?.debug(
-      `_createHook: Creating ${hookType}: ${name}@${version} from ${basedir}`,
+      `_createHook: Creating ${hookType}: ${name}@${version} from ${basedir}`
     );
 
     try {
@@ -373,7 +392,7 @@ class AcroAgent {
       }
     } catch (err: any) {
       this.logger?.error(
-        `_createHook: Error loading ${hookType}: ${err.name}, ${err.message}, ${err.stack}`,
+        `_createHook: Error loading ${hookType}: ${err.name}, ${err.message}, ${err.stack}`
       );
     }
 
@@ -399,9 +418,9 @@ class AcroAgent {
           "RequireHook",
           exports,
           name,
-          basedir,
+          basedir
         );
-      },
+      }
     );
 
     this._importHook = new ImportHook(
@@ -412,9 +431,9 @@ class AcroAgent {
           "ImportHook",
           exports,
           name,
-          basedir,
+          basedir
         );
-      },
+      }
     );
   }
 
